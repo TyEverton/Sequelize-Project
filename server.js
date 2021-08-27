a4f70c795c1ede500477f7b013896efd2c971a2f
Ty Everton <everton_915@yahoo.com>
2021-08-27 12:07:21 -0600

    adding user data

--- server.js
+++ server.js
@@ -4,32 +4,63 @@ const Sequelize = require('sequelize')
 const app = express()
 const port = 3001
 
+const USERS = require('')
+
 const connection = new Sequelize('db', 'user', 'pass', {
-  host: 'localhost', 
+  host: 'localhost',
   dialect: 'sqlite',
   storage: 'db.sqlite',
-  operatorsAliases: false 
+  operatorsAliases: false,
+  define: {
+    freezeTableName: true,
+  },
 })
 
 const User = connection.define('User', {
-  name: Sequelize.STRING,
-  bio: Sequelize.TEXT
+name: Sequelize.STRING,
+email: {
+  type: Sequelize.STRING,
+  validate: {
+    isEmail: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    validate: {
+      isAlphanumeric: true
+    }
+  }
+}
+})
+
+app.get('/', (req, res) => {
+  User.create({
+    name: 'Ty',
+    bio: 'Test bio',
+  })
+    .then((user) => {
+      res.json(user)
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(404).send(error)
+    })
 })
 
 connection
   .sync({
-    logging: console.log
+    logging: console.log,
+    force: true,
   })
   .then(() => {
     User.create({
       name: 'Ty',
-      bio: 'New bio entry'
+      bio: 'New bio entry 1',
     })
   })
   .then(() => {
-    console.log("Connection to database established!")
+    console.log('Connection to database established!')
   })
-  .catch(err => {
+  .catch((err) => {
     console.log('Unable to connect to the database:', err)
   })
 
